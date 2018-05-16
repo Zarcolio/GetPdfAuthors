8789025fb31532cf712e2d98238fb354d24a42ab
Ocraz <registered@quicknet.nl>
2018-05-16 13:52:49 +0200

    Added  --no-check-certificate to wget

--- GetPdfAuthors.sh
+++ GetPdfAuthors.sh
@@ -2,7 +2,7 @@
 
 mkdir $2
 cd $2
-wget -r http://$1.$2 -nd --adjust-extension --content-disposition -e robots=off -p --random-wait -U mozilla --mirror --domains=$2 --tries 2
+wget -r http://$1.$2 -nd --adjust-extension --content-disposition -e robots=off -p --random-wait -U mozilla --mirror --domains=$2 --tries 2 --no-check-certificate
 unzip -jnq "*.zip"
 
 for filename in *.pdf; do
